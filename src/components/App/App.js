9690a504de5c026f28fba1c9cdee39e632500ab2
Fabz <andradefabian@gmail.com>
2023-03-05 18:57:27 -0300

    first commit testigos de hielo

---
+++ src/components/App/App.js
@@ -0,0 +1,154 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Testigos de Hielo</h1>
+      <p>Durante la historia reciente de nuestro planeta ha sido testigo de muchos cambios en sus condiciones del clima y del ambiente. El hielo almacenado en los glaciares, ha visto y ha sufrido estos cambios, los que están almacenados en las capas que va almacenando año a año. Estos cambios se han traducido en el aumento de la temperatura a escala global, que han traído grandes cambios en las condiciones en que vivimos, y que nos han afectado directamente en nuestra vida cotidiana.</p>
+  <h2>1. Cambio climático:</h2>
+  <p>Qué es, efectos del cambio climático a nivel mundial y nacional.
+<ul>
+  <li>
+    Términos importantes: clima y atmósfera.
+  </li>
+  <li>
+    Imágenes e infografía asociada: gráfico concentración de gases en la atmósfera
+  </li>
+</ul>
+De acuerdo con la Naciones Unidas, el cambio climático se define como:  “Los cambios a largo plazo de las temperaturas y los patrones climáticos. Estos cambios pueden ser naturales, por ejemplo, a través de las variaciones del ciclo solar. Pero desde el siglo XIX, las actividades humanas han sido el principal motor del cambio climático, debido principalmente a la quema de combustibles fósiles como el carbón, el petróleo y el gas” (https://www.un.org/es/climatechange/what-is-climate-change)
+
+
+ Gráfico de concentración en partes por millón (ppm) de moléculas de dióxido de carbono en la atmósfera global entre el año 0 y el año 2022 de la era actual. Fuente: https://ourworldindata.org/atmospheric-concentrations. 
+
+
+Gráfico concentración de gases de efecto invernadero a escala global,  desde el año 1750 a 2022. Fuente: Gráfico de elaboración propia basado en los datos de: https://ourworldindata.org/atmospheric-concentrations
+
+
+
+
+
+
+
+
+
+
+La quema de combustibles fósiles genera emisiones de gases de efecto invernadero que actúan como una manta que envuelve a la Tierra, atrapando el calor del sol y elevando las temperaturas (https://www.un.org/es/climatechange/what-is-climate-change)
+
+
+Esquema explicativo del balance de energía generado por los gases de efecto invernadero en la atmósfera. Fuente: http://antarctica.cl/divulgacion/ 
+El cambio climático es una variación de las condiciones ambientales del planeta, ocasionado directa o indirectamente por la actividad humana, el cual está alterando la composición de la atmósfera. Manifestándose en un aumento de las temperaturas medias y alteraciones del clima a escala mundial.
+El clima es un estado característico propio de una zona en un largo periodo de tiempo, incluyendo aspectos como la temperatura, precipitaciones, humedad y viento. Mientras que la atmósfera está formada por una mezcla de gases y participa en la regulación del clima.
+</p>
+<h2>2. Variabilidad e historia del clima:</h2>
+<p>
+
+</p>
+<h2>3.Importancia de la montaña como reserva hídrica
+</h2>
+<p>
+rol de los glaciares e importancia del agua.
+- Términos importantes: glaciares, reserva natural de agua y agua dulce.
+-  Imágenes e infografía asociada: viaje por la cuenca del Aconcagua (3D), mapa de glaciares y redes hídricas de la zona, video de la expedición.
+
+En el presente las consecuencias del proceso de cambio climático son más tangibles que nunca, las que hemos observado concretamente en nuestro territorio. En Chile (y en gran parte de Sudamérica) hemos estado experimentando uno de los periodos de sequías jamás visto a escala histórica (Morales et al., 2020; Bustos et al., 2015), presentándose un déficit importante en la cubierta de nieve para los últimos años que alcanzó un 34% de la cobertura de nieve, respecto al periodo promedio entre 2000 y 2021, en la cuenca del río Aconcagua que surte directamente de agua a la región de Valparaíso (IANIGLA-CONICET y (CR)²; web Observatorio de Nieve en los Andes de Argentina y Chile). Esta condición extrema ha acarreado nefastas consecuencias, como incendios forestales y urbanos, que han afectado directamente a los habitantes de ciudades densamente pobladas como la ciudad de Valparaíso (Sapiains y otros., 2019).
+
+
+
+Mapa de glaciares y redes hidricas, zona central de Chile. Fuente: elaboración propia.
+
+
+Fotografía con orientación nor-oriente del glaciar Juncal Sur, octubre 2022. Fuente: elaboración propia.
+
+
+
+Fotografía con orientación sur-poniente del glaciar Juncal Sur, octubre 2022. Fuente: elaboración propia.
+
+
+En Chile el 70% de la población se abastece de agua proveniente de la cordillera (Bórquez et al., 2006), además nuestro país alberga más del 80% de los glaciares existentes de Sudamérica (Barcaza et al., 2017), la Cordillera de Los Andes aloja a gran cantidad de estos glaciares, los cuales son parte importante de las reservas de agua dulce (torres de agua), fundamentales para la vida y el ecosistema. Los glaciares emplazados en la cordillera contribuyen de gran manera a los flujos de agua superficial como subterráneos, llegando a aportar el 67% del agua dulce en años con bajas precipitaciones (Peña and Nazarala, 1987), sin embargo la principal fuente de recarga sigue siendo la precipitación de nieve y lluvia para las cuencas de Chile central. En particular para la región de Valparaíso, la cuenca del Aconcagua, se alimenta entre de fusión de nieve y lluvia (~50%), fuentes glaciares (~30%) y de aguas subterráneas`y formas periglacieres (~20%) (Crespo et al., 2020).    
+
+Ilustración del ciclo del agua. Fuente: Servicio Geológico de Estados Unidos (https://www.usgs.gov/media/images/el-ciclo-del-agua-water-cycle-spanish-png) 
+
+</p>
+
+<h2>4. Glaciología: definición de glaciología, que estudia y cómo se estudia, además de la importancia de esta ciencia.
+</h2>
+<p> - Imágenes e infografía asociada: videos de la expedición, imagen testigo asociada a eventos históricos, imagen ciclo del agua.
+
+La glaciología es una disciplina científica encargada de estudiar los glaciares, las glaciaciones y la criósfera en general, a través de monitoreos del comportamiento y formación, realizando mediciones en terreno o mediciones remotas.
+La criósfera se define como todos los componentes congelados naturales que se encuentran sobre o debajo de la superficie terrestre u oceánicas. Incluyendo nieve, glaciares, casquetes de hielo icebergs, hielo marino, lagos y ríos congelados, además de suelos congelados. Tiene un rol importante en la regulación del clima a escala local y global, influyendo en la hidrología, nubes, precipitaciones y la circulación de la atmósfera y los océanos. El cambio climático está ocasionando una gran pérdida de la criósfera, mediante el derretimiento de grandes extensiones de hielo.
+
+Componentes de la Criósfera que se estudian comúnmente en los procesos de balance de masa por la glaciología (Fuente: “El Atlas de Glaciares y Aguas Andinos: el impacto del retroceso de los glaciares sobre los recursos hídricos” (Johansen et al., 2019))
+
+Los estudios en terreno incluyen normalmente expediciones en condiciones muy adversas, en donde investigadores hombres y mujeres deben exponerse a condiciones ambientales complejas. Una buena preparación de la expedición y buen equipamiento asegura que los científicos puedan trabajar de buena forma en la montaña o regiones polares. Muchas veces las expediciones ocurren en zonas muy remotas (zonas de acumulación de hielo), que deben ser accedidas caminando por largas rutas sobre glaciares o por medios de transporte como helicóptero o avión.  
+
+Trabajo en terreno de un grupo de glaciólogos que están determinando las tasas de derretimiento de verano en una zona de ablación del glaciar Juncal Norte (Fuente propia).
+</p>
+<h2>5. Qué son los testigos de hielos y para qué sirven.
+</h2>
+<p>
+Imágenes e infografía asociada: video del laboratorio, infografía del testigo del hielo con fotografías de alta resolución de un testigo.
+
+
+
+Los testigos de hielo representan una verdadera diamante de las técnicas científicas, y son una herramienta importante para el estudio glaciar, estos son perforaciones hechas en un glaciar de forma cilíndrica en una capa de hielo o glaciar, realizadas en profundidad desde la superficie. Los glaciares acumulan capas de nieve, que lentamente se transforman en hielo por efecto de la presión y dinámica del glaciar. Cada una de estas capas “captura” las condiciones ambientales del lugar donde el glaciar crece y avanza (temperatura del aire), pero también es un registro único de condiciones ambientales escala global (burbujas de aire que atrapan los gases de la atmósfera), también al ser como una verdadera sábana blanca, los glaciares acumulan impurezas (naturales y generadas por el hombre) que son transportadas desde grandes distancias y que se acumulan con cada nueva precipitación de nieve. Los eventos de erupciones volcánicas son identificados relativamente fácil a lo largo de los testigos de hielo y nos dan una idea de la edad del hielo.
+
+
+Fotografía la erupción del volcán Calbuco, año 2015. Fuente: (https://nuestroclima.com/la-erupcion-del-calbuco-en-4k/)
+
+Fotografía de una sección de testigo de hielo de 200 m de profundidad extraído en 2013, en un punto cercano a la cumbre del monte Hunter, Alaska, Estados Unidos  Fuente: Lecavalier y Markle (2016)
+
+De esta forma la metodología científica utilizada en el análisis de los testigos de hielo ha permitido reconstruir el clima y las condiciones ambientales de hasta 800 mil años (Wolf et al., 2010) que muestran la transición de ciclos glaciares (épocas de climas fríos) e interglaciares (épocas de climas cálidos) y que muestran que el proceso de calentamiento actual no tiene precedentes en el registro del clima del pasado y solo se compara al clima de hace millones de años (
+
+
+
+ 
+Figura resumen de la variación de CO2 reconstruida a partir de datos de testigos de hielo y observaciones actuales (Fuente: https://ourworldindata.org/atmospheric-concentrations )
+
+
+
+
+
+Figura panel superior: resumen de la variación de CO2 y Metano (CH4 reconstruida a partir de datos de testigos de hielo comparadas con observaciones actuales.
+
+Figura panel inferior: resumen de la variación de CO2 comparada con la temperatura atmosférica del planeta reconstruida a partir de información de testigos de hielo y observaciones actuales.
+
+ (Fuente: https://www.bas.ac.uk/data/our-data/publication/ice-cores-and-climate-change/ )
+
+Por lo tanto los testigos aportan información sobre las condiciones ambientales y sus variaciones en el tiempo en cada capa de hielo que compone un glaciar, plasmando información sobre la composición atmosférica y antecedentes de cómo era el ambiente en la época que cayeron las precipitaciones que formaron y forman parte del glaciar, pudiendo así modelar y reconstruir climas pasados.
+
+
+
+Fotografía a escala que muestra un evento de derretimiento en una sección de testigo de hielo de 20 cm de largo en total proveniente del Norte de Chile, representando eventos como olas de calor. Fuente: elaboración propia.
+
+
+
+Fotografía de una sección de testigo de hielo del Norte de Chile, representando transformación o transición de nieve a neviza y finalmente a hielo. Fuente: elaboración propia.
+
+
+
+Ejemplo de infografía de un testigo de hielo de la Antártica relacionado a eventos históricos. Fuente: British Antarctic Survey (https://www.bas.ac.uk/)
+
+</p>
+<h2>6. Modelamiento del futuro (presente próximo siglo)
+</h2>
+<p>Los efectos del cambio climático descritos hasta ahora continuaran en el futuro, a no ser que se tomen medidas urgentes en cuanto a la forma de usar los recursos naturales (Rojas et al., 2019; IPCC, 2021). De esta forma sabemos que las precipitaciones disminuirán entre un 5% y un 15% en la zona centro y sur de Chile (Stehr et al., 2019), lo que inevitablemente repercutirá en la perdida de masa glaciar a lo largo de Chile. En la zona central se proyecta una perdida de masa de hielo de entre un 12% y 37% (Glaciar Universidad) (Escanilla-Minchel et al., 2020) dependiendo de las decisiones socio-políticas que se lleguen a tomar en el futuro. El IPCC (2021) ha fijado como meta un calentamiento no superior a 1.5°C para el presente siglo, de manera de evitar efectos irreversibles en nuestro ambiente debido al cambio climático. Si esta meta se cumple, los efectos del cambio climático, podrián ser moderados pero no desapareceran. Las simulaciones, muestran que en todos los escenarios la precipitación de la zona central disminuirá y la temperatura aumentará (como efectos tangibles del cambio climático). 
+  
+
+
+
+
+
+  Figura panel superior modelo de las tendencias de precipitaciones (IPCC, 2021) para el mundo en caso de un aumento de 1.5°C, 2°C y 4°C 
+  Figura panel superior modelo de las tendencias de temperaturas regionales (IPCC, 2021) para el mundo en caso de un aumento promedio de 1.5°C, 2°C y 4°C de la temperartura.
+  
+  
+  Modelamiento de la temperatura a escala global bajo distintos escenarios socio-económicos. Fuente: http://antarctica.cl/divulgacion/ 
+  
+  De acuerdo con las palabras del Dr. Raul Cordero (www.antarctica.cl): El clima del futuro depende de las decisiones que tomemos hoy. Si nos comprometemos con los objetivos del Acuerdo de París podríamos limitar el alza de la temperatura global a alrededor de 1.5°C y lograríamos conservar gran parte de la biodiversidad del planeta. Si no lo hacemos y no redoblamos esfuerzos para mitigar nuestras emisiones, el planeta podría ser a fines de siglo hasta 4°C en promedio más cálido que hoy, condenando a la extinción a muchas especies animales y vegetales. Es  nuestra tarea exigir que se cumplan estos acuerdos, de lo contrario, ya conocemos los posibles escenarios futuros.
+  
+  </p>
+    </div>
+  )
+}
+
+export default App
